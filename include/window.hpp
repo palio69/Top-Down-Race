29c8fadd9f2ec01397a844c8932974e9756c057e
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-05-12 20:47:17 -0300

    reboot

--- include/window.hpp
+++ include/window.hpp
@@ -3,52 +3,25 @@
 
 
 #include <SDL2/SDL.h>
-#include <image.hpp>
-#include <area.hpp>
 
 
 
 class window {
 private:
-	SDL_Window* win;
-	SDL_Renderer* ren;
-	const int w, h, flags;
+  SDL_Window* win;
+  SDL_Renderer* ren;
 
 public:
-	window(const char* title, const int w, const int h, const int flags = SDL_WINDOW_SHOWN) :
-		win(nullptr), ren(nullptr), w(w), h(h), flags(flags) {
-		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, this->w, this->h, this->flags);
-		this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
-	}
-	window(const char* title, const window& wnd) :
-		win(nullptr), ren(nullptr), w(wnd.get_w()), h(wnd.get_h()), flags(wnd.get_flags()) {
-		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, this->w, this->h, this->flags);
-		this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
-	}
-	window(const char* title, const window&& wnd) :
-		win(nullptr), ren(nullptr), w(wnd.get_w()), h(wnd.get_h()), flags(wnd.get_flags()) {
-		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, this->w, this->h, this->flags);
-		this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
-	}
-	~window() {
-		SDL_DestroyWindow(this->win);
-		SDL_DestroyRenderer(this->ren);
-	}
+  window(const char* title, const int w, const int h) :
+    win(nullptr), ren(nullptr) {
+    this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_SHOWN);
+    this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
+  }
+  ~window() {
+    SDL_DestroyWindow(this->win);
+    SDL_DestroyRenderer(this->ren);
+  }
 
-	SDL_Window* get_win() const { return this->win; }
-	SDL_Renderer* get_ren() const { return this->ren; }
-	int get_w() const { return this->w; }
-	int get_h() const { return this->h; }
-	int get_flags() const { return this->flags; }
-
-	void clear() const;
-	void clear(const int r, const int g, const int b) const;
-	void clear(const int r, const int g, const int b, const int a) const;
-
-	void render(const image& img) const;
-	void render(const area_manager& area_man) const { area_man.render(this->ren); }
-	void render(const renderer& rnd) const { rnd.render(this->ren); }
-
-	void update() const;
-
+  void clear() const { SDL_RenderClear(this->ren); }
+  void update() const { SDL_RenderPresent(this->ren); }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
-#include <game.hpp>
+#include <window.hpp>
 
-
+#define W 720
+#define H 480
 
 int main(int argc, char* argv[]) {
-	game* g = new game("Klonoa Dimensions");
-	g->play();
-	delete g;
-	return 0;
+  SDL_Init(SDL_INIT_EVERYTHING);
+  
+  window win("Top-Down Race", W, H);
+
+  bool running = true;
+  SDL_Event evn;
+  
+  while(running) {
+
+    while(SDL_PollEvent(&evn)) {
+
+      if (evn.type == SDL_QUIT)
+	running = false;
+
+    }
+
+    win.clear();
+    win.update();
+
+  }
+
+  SDL_Quit();
+  return 0;
 }
