e5de78ec47c23c015bf773d011c193817eb97c12
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-06-03 14:09:10 -0300

    Updated "player.max_speed" attribute

--- src/game.cpp
+++ src/game.cpp
@@ -63,7 +63,7 @@ struct {
 
   const vec2f pos = { win.fw / 2 - fw / 2, win.fh / 2 - fh / 2 };
   const float speed = 0.0f,
-    max_speed = 50.0f;
+    max_speed = 100.0f;
   const double angle = 0.0;
 
 } player;
