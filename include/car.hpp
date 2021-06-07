fb7ec67608bce3b85ace917cff072ece81c1aac6
mine-cpp <paul0eiry0@gmail.com>
2021-06-07 14:55:38 -0300

    Added "car::cam" attribute

--- include/car.hpp
+++ include/car.hpp
@@ -4,6 +4,7 @@
 #include <SDL2/SDL.h>
 #include <vec2f.hpp>
 #include <image.hpp>
+#include <camera.hpp>
 
 class car {
 private:
@@ -12,6 +13,7 @@ private:
     goal_speed;
   const float max_speed;
   double angle;
+  camera cam;
 
   image sprite;
   const SDL_RendererFlip flip;
@@ -28,10 +30,13 @@ public:
     pos(pos),
     speed(speed), goal_speed(0.0f), max_speed(max_speed),
     angle(angle),
+    cam(pos),
 
     sprite(sprite),
     flip(flip) { }
   ~car() { SDL_DestroyTexture(this->sprite.tex); }
+
+  camera get_cam() const { return this->cam; }
 
   void update(const Uint8* key, const float delta_time);
   void render(SDL_Renderer* renderer) const;
