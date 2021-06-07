399d0b190cde811d7f71fe30a9aa80f12a6f1f95
mine-cpp <paul0eiry0@gmail.com>
2021-06-07 08:22:49 -0300

    Added overloads for arithmetic and assignment operators in "vec2f" class

--- include/vec2f.hpp
+++ include/vec2f.hpp
@@ -2,6 +2,39 @@
 
 #include <iostream>
 
-struct vec2f { float x, y; };
 
-std::ostream& operator << (std::ostream& os, const vec2f vec);
+
+struct vec2f {
+  float x, y;
+
+  vec2f& operator =(const vec2f vec);
+  vec2f& operator +=(const vec2f vec);
+  vec2f& operator -=(const vec2f vec);
+  vec2f& operator *=(const vec2f vec);
+  vec2f& operator /=(const vec2f vec);
+
+};
+
+
+
+inline constexpr vec2f operator +(const vec2f vec1, const vec2f vec2) {
+  const vec2f vec3 = { (vec1.x + vec2.x), (vec1.y + vec2.y) };
+  return vec3;
+}
+
+inline constexpr vec2f operator -(const vec2f vec1, const vec2f vec2) {
+  const vec2f vec3 = { (vec1.x - vec2.x), (vec1.y - vec2.y) };
+  return vec3;
+}
+
+inline constexpr vec2f operator *(const vec2f vec1, const vec2f vec2) {
+  const vec2f vec3 = { (vec1.x * vec2.x), (vec1.y * vec2.y) };
+  return vec3;
+}
+
+inline constexpr vec2f operator /(const vec2f vec1, const vec2f vec2) {
+  const vec2f vec3 = { (vec1.x / vec2.x), (vec1.y / vec2.y) };
+  return vec3;
+}
+
+std::ostream& operator <<(std::ostream& os, const vec2f vec);
--- src/vec2f.cpp
+++ src/vec2f.cpp
@@ -1,5 +1,35 @@
 #include <vec2f.hpp>
 
+vec2f& vec2f::operator =(const vec2f vec) {
+  this->x = vec.x;
+  this->y = vec.y;
+  return *this;
+}
+
+vec2f& vec2f::operator +=(const vec2f vec) {
+  this->x += vec.x;
+  this->y += vec.y;
+  return *this;
+}
+
+vec2f& vec2f::operator -=(const vec2f vec) {
+  this->x -= vec.x;
+  this->y -= vec.y;
+  return *this;
+}
+
+vec2f& vec2f::operator *=(const vec2f vec) {
+  this->x *= vec.x;
+  this->y *= vec.y;
+  return *this;
+}
+
+vec2f& vec2f::operator /=(const vec2f vec) {
+  this->x /= vec.x;
+  this->y /= vec.y;
+  return *this;
+}
+
 std::ostream& operator <<(std::ostream& os, const vec2f vec) {
   os << "{ " << vec.x << ", " << vec.y << " }";
   return os;
