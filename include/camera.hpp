d8bc74080088817b087752e49f9ded8158584f69
mine-cpp <paul0eiry0@gmail.com>
2021-06-09 08:46:14 -0300

    Updated "vec2f" operators

--- include/camera.hpp
+++ include/camera.hpp
@@ -4,14 +4,25 @@
 
 class camera {
 private:
-  vec2f ref_pos;
-  vec2f camera_pos;
+  const vec2f xy_limit, wh_limit,
+    window_wh;
+  vec2f ref_pos,
+    camera_pos;
 
 public:
-  camera(const vec2f ref_pos) :
-    ref_pos(ref_pos), camera_pos { 0.0f, 0.0f } { }
+  camera(
+	 const vec2f xy_limit, const vec2f wh_limit,
+	 const vec2f window_wh,
+	 const vec2f ref_pos,
+	 const vec2f camera_pos = { 0.0f, 0.0f }
+	 ) :
+    xy_limit(xy_limit), wh_limit(wh_limit),
+    window_wh(window_wh),
+    ref_pos(ref_pos),
+    camera_pos(camera_pos) { }
 
   vec2f get_camera_pos() const { return this->camera_pos; }
+
   void update(const vec2f new_ref);
 
 };
--- src/camera.cpp
+++ src/camera.cpp
@@ -1,7 +1,26 @@
 #include <camera.hpp>
 
 void camera::update(const vec2f new_ref) {
-  const vec2f distance = new_ref - this->ref_pos;
-  this->ref_pos = new_ref;
-  this->camera_pos += distance;
+  const vec2f refs_distance = new_ref - this->ref_pos,
+    new_camera_pos = this->camera_pos + refs_distance,
+    new_camera_wh = new_camera_pos + this->window_wh,
+
+    xy2_limit = this->xy_limit + this->wh_limit,
+    xy2_distance = xy2_limit - this->window_wh;
+
+  vec2f final_camera_pos = new_camera_pos;
+
+
+
+  if (new_camera_pos < this->xy_limit)
+    final_camera_pos = this->xy_limit;
+
+  else if (new_camera_wh > xy2_limit)
+    final_camera_pos = xy2_distance;
+
+
+
+  this->camera_pos = final_camera_pos;
+  this->ref = new_ref;
+
 }
