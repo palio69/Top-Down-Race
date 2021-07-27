52ba28d075642a9495399454bc207acd3e1c62aa
mine-cpp <paul0eiry0@gmail.com>
2021-07-27 10:55:13 -0300

    anoda fard commit69

--- src/system.cpp
+++ src/system.cpp
@@ -3,6 +3,22 @@
 
 
 
+float time_system::current_time__;
+float time_system::next_time__;
+float time_system::delta_time__;
+
+void time_system::init() {
+  current_time__ = time();
+  next_time__ = 0.0f;
+  delta_time__ = 0.0f;
+}
+
+void time_system::work() {
+  next_time__ = time();
+  delta_time__ = next_time__ - current_time__;
+  current_time__ = next_time__;
+}
+
 bool event_system::running__, event_system::pause__;
 float event_system::counter__;
 Uint8* event_system::key__;
@@ -119,22 +135,4 @@ void picture_system::work() {
 
   }
 
-}
-
-
-
-float time_system::current_time__;
-float time_system::next_time__;
-float time_system::delta_time__;
-
-void time_system::init() {
-  current_time__ = time();
-  next_time__ = 0.0f;
-  delta_time__ = 0.0f;
-}
-
-void time_system::work() {
-  next_time__ = time();
-  delta_time__ = next_time__ - current_time__;
-  current_time__ = next_time__;
 }
